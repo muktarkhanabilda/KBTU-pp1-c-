0b6e8f1cee41dd76a1233ddb9a66039a5e06032a
muktarkhanabilda <117581921+muktarkhanabilda@users.noreply.github.com>
2022-11-06 17:17:47 +0300

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int num;
+	double ans; 
+	cin >> num;
+	ans=sqrt(num);
+	cout << ans << endl;
+}
+	
